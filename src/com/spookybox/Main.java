d264fa27c0b325276f1ee243eedfd4d9911d72b9
Jonathan Johnson <jonathanjohnson@iheartmedia.com>
2016-07-08 16:46:21 -0400

    initial commit

---
+++ src/com/spookybox/Main.java
@@ -0,0 +1,14 @@
+package com.spookybox;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+        //demo.pack();
+        //demo.setVisible(true);
+
+        XYChart chart = new XYChart();
+        chart.pack();
+        chart.setVisible(true);
+    }
+}
